b3eefc9a7cf55c75b2dbd9d3c0cc59926c36d1f0
jtlap <jtlapreste@gmail.com>
2017-03-17 12:05:26 +0100

    complex sqrt for real input bench

--- bench/type/complex/simd/sqrt/real.float.m10_10.cpp
+++ bench/type/complex/simd/sqrt/real.float.m10_10.cpp
@@ -6,27 +6,18 @@
 //                            http://www.boost.org/LICENSE_1_0.txt
 // -------------------------------------------------------------------------------------------------
 
-/// bench for functor sqr_abs in std mode for float type with no decorator (regular call).
 #include <simd_bench.hpp>
 #include <boost/simd/pack.hpp>
 #include <boost/simd/type/complex.hpp>
-#include <boost/simd/type/complex/function/sqr_abs.hpp>
+#include <boost/simd/type/complex/function/sqrt.hpp>
 
 namespace nsb = ns::bench;
 namespace bs =  boost::simd;
 
-struct sqr_abs_std
-{
-  template < typename T >
-  BOOST_FORCEINLINE T operator()(const std::complex<T> & a0) const
-  {
-    return std::norm(a0);
-  }
-};
-
 DEFINE_BENCH_MAIN()
 {
-  using T = std::complex<double>;
-  run<T>(sqr_abs_std(), nsbg::rand<T>(-10, 10, -10, 10));
+  using T = bs::pack<float>;
+  run<T>(bs::cmplx::sqrt, nsbg::rand<T>(-10, 10));
 }
+
 

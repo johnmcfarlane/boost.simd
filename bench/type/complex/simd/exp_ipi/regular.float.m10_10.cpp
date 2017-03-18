f29bbd70792b57eaf5d1acb235204b0bc56471dd
jtlap <jtlapreste@gmail.com>
2017-03-18 10:19:51 +0100

    simd benches for exp_i expm_i mul_i and mul_mi

---
+++ bench/type/complex/simd/exp_ipi/regular.float.m10_10.cpp
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2016 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+#include <simd_bench.hpp>
+#include <boost/simd/pack.hpp>
+#include <boost/simd/type/complex.hpp>
+#include <boost/simd/type/complex/function/exp_ipi.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_BENCH_MAIN()
+{
+  using T = bs::complex<bs::pack<float>>;
+  run<T>(bs::exp_ipi, nsbg::rand<T>(-10, 10, -10, 10));
+}
+
+
---
+++ bench/type/complex/simd/mul_mi/real.float.m10_10.cpp
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2016 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+#include <simd_bench.hpp>
+#include <boost/simd/pack.hpp>
+#include <boost/simd/type/complex.hpp>
+#include <boost/simd/type/complex/function/mul_mi.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_BENCH_MAIN()
+{
+  using T = bs::pack<float>;
+  run<T>(bs::mul_mi, nsbg::rand<T>(-10, 10));
+}
+
+

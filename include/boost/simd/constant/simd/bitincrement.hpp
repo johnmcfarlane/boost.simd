0679d6380e5399d4a50ece9256e985d8c269504b
jtlap <jtlapreste@gmail.com>
2017-05-20 11:00:11 +0200

    bitincrement

--- include/boost/simd/constant/simd/bitincrement.hpp
+++ include/boost/simd/constant/simd/bitincrement.hpp
@@ -11,6 +11,7 @@
 #define BOOST_SIMD_CONSTANT_SIMD_BITINCREMENT_HPP_INCLUDED
 
 #include <boost/simd/constant/scalar/bitincrement.hpp>
-#include <boost/simd/arch/common/simd/constant/constant_value.hpp>
+#include <boost/simd/arch/common/generic/constant/bitincrement.hpp>
+
 
 #endif

63bb2d90096a03dc9692423129b1c3f64a94d2cb
jtlap <jtlapreste@gmail.com>
2017-06-02 09:16:59 +0200

    cotd

--- include/boost/simd/function/definition/cotd.hpp
+++ include/boost/simd/function/definition/cotd.hpp
@@ -12,25 +12,11 @@
 #define BOOST_SIMD_FUNCTION_DEFINITION_COTD_HPP_INCLUDED
 
 #include <boost/simd/config.hpp>
-#include <boost/simd/detail/dispatch/function/make_callable.hpp>
-#include <boost/simd/detail/dispatch/hierarchy/functions.hpp>
-#include <boost/simd/detail/dispatch.hpp>
+#include <boost/simd/detail/overload.hpp>
 
 namespace boost { namespace simd
 {
-  namespace tag
-  {
-    BOOST_DISPATCH_MAKE_TAG(ext, cotd_, boost::dispatch::elementwise_<cotd_>);
-  }
-
-  namespace ext
-  {
-    BOOST_DISPATCH_FUNCTION_DECLARATION(tag, cotd_)
-  }
-
-  BOOST_DISPATCH_CALLABLE_DEFINITION(tag::cotd_,cotd);
-
-
+  BOOST_SIMD_MAKE_CALLABLE(cotd_,cotd);
 } }
 
 #endif

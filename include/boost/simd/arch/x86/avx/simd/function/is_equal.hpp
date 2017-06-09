75bd509b3dd14cf30f0f2b7cfc0f454c4f9109ae
jtlap <jtlapreste@gmail.com>
2017-06-09 17:02:24 +0200

    avx is_equal

--- include/boost/simd/arch/x86/avx/simd/function/is_equal.hpp
+++ include/boost/simd/arch/x86/avx/simd/function/is_equal.hpp
@@ -9,40 +9,38 @@
 #ifndef BOOST_SIMD_ARCH_X86_AVX_SIMD_FUNCTION_IS_EQUAL_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_X86_AVX_SIMD_FUNCTION_IS_EQUAL_HPP_INCLUDED
 
-#include <type_traits>
-#include <boost/simd/pack.hpp>
-#include <boost/simd/meta/as_logical.hpp>
+#include <boost/simd/detail/pack.hpp>
 
-namespace boost { namespace simd { namespace ext
+namespace boost { namespace simd { namespace detail
 {
-  BOOST_FORCEINLINE pack<double,4,avx_> plus_( BOOST_SIMD_SUPPORTS(avx_)
-                                             , pack<double,4,avx_> const& a0
-                                             , pack<double,4,avx_> const& a1
-                                             ) BOOST_NOEXCEPT
+  BOOST_FORCEINLINE pack<double,4,avx_> is_equal_( BOOST_SIMD_SUPPORTS(avx_)
+                                                 , pack<double,4,avx_> const& a0
+                                                 , pack<double,4,avx_> const& a1
+                                                 ) BOOST_NOEXCEPT
   {
     return _mm256_cmp_pd(a0,a1,CMP_EQ_OQ);
   }
 
-  BOOST_FORCEINLINE pack<float,8,avx_> plus_ ( BOOST_SIMD_SUPPORTS(avx_)
-                                             , pack<float,8,avx_> const& a0
-                                             , pack<float,8,avx_> const& a1
-                                             ) BOOST_NOEXCEPT
+  BOOST_FORCEINLINE pack<float,8,avx_> is_equal_ ( BOOST_SIMD_SUPPORTS(avx_)
+                                                 , pack<float,8,avx_> const& a0
+                                                 , pack<float,8,avx_> const& a1
+                                                 ) BOOST_NOEXCEPT
   {
     return _mm256_cmp_ps(a0,a1,_CMP_EQ_OQ);
   }
 
-  BOOST_FORCEINLINE pack<double,2,avx_> plus_( BOOST_SIMD_SUPPORTS(avx_)
-                                             , pack<double,2,avx_> const& a0
-                                             , pack<double,2,avx_> const& a1
-                                             ) BOOST_NOEXCEPT
+  BOOST_FORCEINLINE pack<double,2,sse_> is_equal_( BOOST_SIMD_SUPPORTS(sse_)
+                                                 , pack<double,2,sse_> const& a0
+                                                 , pack<double,2,sse_> const& a1
+                                                 ) BOOST_NOEXCEPT
   {
     return _mm_cmp_pd(a0,a1,CMP_EQ_OQ);
   }
 
-  BOOST_FORCEINLINE pack<float,4,avx_> plus_ ( BOOST_SIMD_SUPPORTS(avx_)
-                                             , pack<float,4,avx_> const& a0
-                                             , pack<float,4,avx_> const& a1
-                                             ) BOOST_NOEXCEPT
+  BOOST_FORCEINLINE pack<float,4,sse_> is_equal_ ( BOOST_SIMD_SUPPORTS(sse_)
+                                                 , pack<float,4,sse_> const& a0
+                                                 , pack<float,4,sse_> const& a1
+                                                 ) BOOST_NOEXCEPT
   {
     return _mm_cmp_ps(a0,a1,_CMP_EQ_OQ);
   }

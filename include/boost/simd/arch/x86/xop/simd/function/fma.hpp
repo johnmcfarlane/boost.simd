8b000d1f8d4c079d5a2af803f4ad6c66e2f50b04
jtlap <jtlapreste@gmail.com>
2017-06-10 08:21:15 +0200

    xop fma

--- include/boost/simd/arch/x86/xop/simd/function/fma.hpp
+++ include/boost/simd/arch/x86/xop/simd/function/fma.hpp
@@ -9,46 +9,46 @@
 #ifndef BOOST_SIMD_ARCH_X86_XOP_SIMD_FUNCTION_FMA_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_X86_XOP_SIMD_FUNCTION_FMA_HPP_INCLUDED
 
+#include <boost/simd/detail/pack.hpp>
 #include <boost/simd/detail/overload.hpp>
-#include <boost/simd/detail/meta/size_picker.hpp>
 
 #if BOOST_HW_SIMD_X86_AMD_XOP
 namespace boost { namespace simd { namespace detail
 {
   template < typename T>
-  BOOST_FORCEINLINE pack<std::int32_t,4,sse_> xfma_( pack<std::int32_t,4,sse_> const& a0
-                                                   , pack<std::int32_t,4,sse_> const& a1
-                                                   , pack<std::int32_t,4,sse_> const& a2
-                                                   , std::true_type const &
+  BOOST_FORCEINLINE pack<std::int32_t,4,sse_> fma_(BOOST_SIMD_SUPPORTS(sse2_)
+                                                  , pack<std::int32_t,4,sse_> const& a0
+                                                  , pack<std::int32_t,4,sse_> const& a1
+                                                  , pack<std::int32_t,4,sse_> const& a2
+                                                  ) BOOST_NOEXCEPT //32 bits
+  {
+    return _mm_macc_epi32(a0, a1, a2);
+  }
+
+  template < typename T>
+  BOOST_FORCEINLINE pack<std::int16_t,8,sse_> fma_(BOOST_SIMD_SUPPORTS(sse2_)
+                                                  , pack<std::int16_t,8,sse_> const& a0
+                                                  , pack<std::int16_t,8,sse_> const& a1
+                                                  , pack<std::int16_t,8,sse_> const& a2
+                                                  ) BOOST_NOEXCEPT //16 bits
+  {
+    return  _mm_macc_epi16(a0, a1, a2);
+  }
+  template < typename T>
+  BOOST_FORCEINLINE pack<std::uint32_t,4,sse_> fma_(BOOST_SIMD_SUPPORTS(sse2_)
+                                                   , pack<std::uint32_t,4,sse_> const& a0
+                                                   , pack<std::uint32_t,4,sse_> const& a1
+                                                   , pack<std::uint32_t,4,sse_> const& a2
                                                    ) BOOST_NOEXCEPT //32 bits
   {
     return _mm_macc_epi32(a0, a1, a2);
   }
 
   template < typename T>
-  BOOST_FORCEINLINE pack<std::int16_t,8,sse_> xfma_( pack<std::int16_t,8,sse_> const& a0
-                                                   , pack<std::int16_t,8,sse_> const& a1
-                                                   , pack<std::int16_t,8,sse_> const& a2
-                                                   , std::false_type const &
-                                                   ) BOOST_NOEXCEPT //16 bits
-  {
-    return  _mm_macc_epi16(a0, a1, a2);
-  }
-  template < typename T>
-  BOOST_FORCEINLINE pack<std::uint32_t,4,sse_> xfma_( pack<std::uint32_t,4,sse_> const& a0
-                                                    , pack<std::uint32_t,4,sse_> const& a1
-                                                    , pack<std::uint32_t,4,sse_> const& a2
-                                                    , std::true_type const &
-                                                    ) BOOST_NOEXCEPT //32 bits
-  {
-    return _mm_macc_epi32(a0, a1, a2);
-  }
-
-  template < typename T>
-  BOOST_FORCEINLINE pack<std::uint16_t,8,sse_> xfma_( pack<std::uint16_t,8,sse_> const& a0
+  BOOST_FORCEINLINE pack<std::uint16_t,8,sse_> fma_(BOOST_SIMD_SUPPORTS(sse2_)
+                                                    ,pack<std::uint16_t,8,sse_> const& a0
                                                     , pack<std::uint16_t,8,sse_> const& a1
                                                     , pack<std::uint16_t,8,sse_> const& a2
-                                                    , std::false_type const &
                                                     ) BOOST_NOEXCEPT //16 bits
   {
     return  _mm_macc_epi16(a0, a1, a2);

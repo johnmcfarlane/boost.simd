36b46135d0b53b3c91ce0b7de89a131b0cdd5a71
jtlap <jtlapreste@gmail.com>
2017-06-11 12:46:45 +0200

    gammln simd

--- include/boost/simd/function/simd/gammaln.hpp
+++ include/boost/simd/function/simd/gammaln.hpp
@@ -13,10 +13,10 @@
 #include <boost/simd/function/scalar/gammaln.hpp>
 #include <boost/simd/arch/common/simd/function/gammaln.hpp>
 
-// #if defined(BOOST_HW_SIMD_X86_OR_AMD_AVAILABLE)
-// #  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_SSE2_VERSION
-// #    include <boost/simd/arch/x86/sse2/simd/function/gammaln.hpp>
-// #  endif
-// #endif
+#if defined(BOOST_HW_SIMD_X86_OR_AMD_AVAILABLE)
+#  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_SSE2_VERSION
+#    include <boost/simd/arch/x86/sse2/simd/function/gammaln.hpp>
+#  endif
+#endif
 
 #endif

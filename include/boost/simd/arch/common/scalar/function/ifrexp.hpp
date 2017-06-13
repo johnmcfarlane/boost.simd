4b8c005c527457d61a86f39df9dd16cb466ccdf0
jtlap <jtlapreste@gmail.com>
2017-06-13 20:43:15 +0200

    ifrexp

--- include/boost/simd/arch/common/scalar/function/ifrexp.hpp
+++ include/boost/simd/arch/common/scalar/function/ifrexp.hpp
@@ -9,7 +9,6 @@
 #ifndef BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_IFREXP_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_IFREXP_HPP_INCLUDED
 
-#include <boost/simd/detail/dispatch/function/overload.hpp>
 #include <boost/simd/function/pedantic.hpp>
 #include <boost/simd/function/std.hpp>
 
@@ -37,104 +36,91 @@
 #include <boost/config.hpp>
 #include <utility>
 #include <cmath>
+#include <boost/simd/detail/meta/convert_helpers.hpp>
 
-namespace boost { namespace simd { namespace ext
+namespace boost { namespace simd { namespace detail
 {
-  namespace bd = boost::dispatch;
+  template < typename T> using pair_it = std::pair<T, si_t<T>>;
 
-  BOOST_DISPATCH_OVERLOAD ( ifrexp_
-                          , (typename A0)
-                          , bd::cpu_
-                          , bs::pedantic_tag
-                          , bd::scalar_< bd::floating_<A0> >
-                          )
+  // raw case
+  template<typename T>
+  BOOST_FORCEINLINE pair_it<T>
+  ifrexp_(BOOST_SIMD_SUPPORTS(cpu_)
+        , raw_tag const &
+         , T a0) BOOST_NOEXCEPT
   {
-    using i_t = bd::as_integer_t<A0, signed>;
-    BOOST_FORCEINLINE std::pair<A0,i_t> operator()(const pedantic_tag &, A0 a0) const BOOST_NOEXCEPT
-    {
-      if (a0 == 0 || is_invalid(a0))
-      {
-        return {a0, i_t(0)};
-      }
-      else
-      {
-        auto m1f  = Mask1frexp<A0>();
-        auto nmb  = Nbmantissabits<A0>();
+    using r2_t = si_t<T>;
+    auto m1f  = Mask1frexp<T>();
+    auto r1   = bitwise_cast<r2_t>(bitwise_and(m1f, a0));
+    auto    x   = bitwise_andnot(a0, m1f);
 
-        i_t r1    = simd::bitwise_cast<i_t>(bitwise_and(m1f, a0));  // extract exp.
+    return  pair_it<T>{ bitwise_or(x,Mask2frexp<T>())
+        , shr(r1,Nbmantissabits<T>()) - Maxexponentm1<T>()
+        };
+  }
+
+  // regular case
+  template<typename T>
+  BOOST_FORCEINLINE pair_it<T>
+  ifrexp_(BOOST_SIMD_SUPPORTS(cpu_)
+         , T a0) BOOST_NOEXCEPT
+  {
+    return a0 ? raw_(simd::ifrexp)(a0) : pair_it<T>{0, 0};
+  }
+
+
+  // pedantic case
+  template<typename T>
+  BOOST_FORCEINLINE pair_it<T>
+  ifrexp_(BOOST_SIMD_SUPPORTS(cpu_)
+         , pedantic_tag const &
+         , T a0) BOOST_NOEXCEPT
+  {
+    using r2_t = si_t<T>;
+    if (a0 == 0 || is_invalid(a0))
+    {
+      return pair_it<T>{a0, 0};
+    }
+    else
+    {
+      auto m1f  = Mask1frexp<T>();
+      auto nmb  = Nbmantissabits<T>();
+
+      r2_t r1 = simd::bitwise_cast<r2_t>(bitwise_and(m1f, a0));  // extract exp.
 
 #ifndef BOOST_SIMD_NO_DENORMALS
-        i_t t = i_t(0);
-        if(is_eqz(r1)) // denormal
-        {
-          a0 *= Twotonmb<A0>();
-          r1  = simd::bitwise_cast<i_t>(bitwise_and(m1f, a0));  // extract exp. again
-          t   = nmb;
-        }
+      r2_t t(0);
+      if(is_eqz(r1)) // denormal
+      {
+        a0 *= Twotonmb<T>();
+        r1  = simd::bitwise_cast<r2_t>(bitwise_and(m1f, a0));  // extract exp. again
+        t   = nmb;
+      }
 #endif
 
-        A0 x  = bitwise_andnot(a0, m1f);               // clear exp. in a0
-        r1 = shr(r1,nmb)- Maxexponentm1<A0>();         // compute exp.
-        if (r1 > Limitexponent<A0>()) return {a0, i_t(0)};
+      T x  = bitwise_andnot(a0, m1f);               // clear exp. in a0
+      r1 = shr(r1,nmb)- Maxexponentm1<T>();         // compute exp.
+      if (r1 > Limitexponent<T>()) return pair_it<T>{a0, 0};
 
 #ifndef BOOST_SIMD_NO_DENORMALS
-        r1 -= t;
+      r1 -= t;
 #endif
 
-        return {bitwise_or(x,Mask2frexp<A0>()), r1};
-      }
+      return {bitwise_or(x,Mask2frexp<T>()), r1};
     }
-  };
+  }
 
-  BOOST_DISPATCH_OVERLOAD ( ifrexp_
-                          , (typename A0)
-                          , bd::cpu_
-                          , bd::scalar_< bd::floating_<A0> >
-                          )
+  template<typename T>
+  BOOST_FORCEINLINE pair_it<T>
+  ifrexp_(BOOST_SIMD_SUPPORTS(cpu_)
+         , std_tag const &
+         , T a0) BOOST_NOEXCEPT
   {
-    using i_t = bd::as_integer_t<A0, signed>;
-    BOOST_FORCEINLINE std::pair<A0,i_t> operator()(A0 a0) const BOOST_NOEXCEPT
-    {
-      if(!a0) return {A0(0),i_t(0)};
-      else    return raw_(simd::ifrexp)(a0);
-    }
-  };
+    int e(0);
+    T r = std::frexp(a0, &e);
+    return pair_it<T>{r, e};
+  }
 
-  BOOST_DISPATCH_OVERLOAD ( ifrexp_
-                          , (typename A0)
-                          , bd::cpu_
-                          , boost::simd::raw_tag
-                          , bd::scalar_< bd::floating_<A0> >
-                          )
-  {
-    using i_t = bd::as_integer_t<A0, signed>;
-    BOOST_FORCEINLINE std::pair<A0,i_t> operator()(const raw_tag &,  A0 a0) const BOOST_NOEXCEPT
-    {
-      auto m1f  = Mask1frexp<A0>();
-      auto r1   = bitwise_cast<i_t>(bitwise_and(m1f, a0));
-      A0    x   = bitwise_andnot(a0, m1f);
-
-      return  { bitwise_or(x,Mask2frexp<A0>())
-              , shr(r1,Nbmantissabits<A0>()) - Maxexponentm1<A0>()
-              };
-    }
-  };
-
-  BOOST_DISPATCH_OVERLOAD ( ifrexp_
-                          , (typename A0)
-                          , bd::cpu_
-                          , boost::simd::std_tag
-                          , bd::scalar_< bd::floating_<A0> >
-                          )
-  {
-    using i_t = bd::as_integer_t<A0, signed>;
-    BOOST_FORCEINLINE std::pair<A0,i_t> operator()(const std_tag &, A0 a0) const BOOST_NOEXCEPT
-    {
-      int e(0);
-      A0 r = std::frexp(a0, &e);
-      return {r, i_t(e)};
-    }
-  };
 } } }
 
 #endif
--- include/boost/simd/arch/common/simd/function/ifrexp.hpp
+++ include/boost/simd/arch/common/simd/function/ifrexp.hpp
@@ -9,6 +9,7 @@
 #ifndef BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_IFREXP_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_IFREXP_HPP_INCLUDED
 
+#include <boost/simd/detail/pack.hpp>
 #include <boost/simd/detail/overload.hpp>
 #include <boost/simd/function/pedantic.hpp>
 #include <boost/simd/function/raw.hpp>
@@ -40,85 +41,92 @@
 #include <boost/simd/constant/twotonmb.hpp>
 #endif
 
-namespace boost { namespace simd { namespace ext
+namespace boost { namespace simd { namespace detail
 {
-  namespace bd = boost::dispatch;
-  namespace bs = boost::simd;
 
-  BOOST_DISPATCH_OVERLOAD ( ifrexp_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pedantic_tag
-                          , bs::pack_<bd::floating_<A0>, X>
-                          )
+  //================================================================================================
+  // raw case
+  template<typename T, std::size_t N
+           ,  typename =  typename std::enable_if<std::is_floating_point<T>::value>
+  >
+  BOOST_FORCEINLINE pair_it<pack<T,N>>
+  ifrexp_(BOOST_SIMD_SUPPORTS(simd_)
+        , raw_tag const&
+        , pack<T,N> const& a0) BOOST_NOEXCEPT
   {
-    using i_t = bd::as_integer_t<A0, signed>;
-    BOOST_FORCEINLINE std::pair<A0,i_t> operator()(pedantic_tag const&, A0 const& a0) const
-    {
-      using s_type = typename A0::value_type;
+    using p_t = pack<T,N>;
+    auto m1f = Mask1frexp<p_t>();
+    auto r1 = bitwise_cast<si_t<p_t>>(bitwise_and(m1f, a0));
+    auto x  = bitwise_andnot(a0, m1f);
 
+    return  pair_it<pack<T,N>>{ bitwise_or(x,Mask2frexp<p_t>())
+        , shr(r1,Nbmantissabits<T>()) - Maxexponentm1<p_t>()
+        };
+  }
+
+  //================================================================================================
+  // regular case
+  template<typename T, std::size_t N
+           ,  typename =  typename std::enable_if<std::is_floating_point<T>::value>
+  >
+  BOOST_FORCEINLINE pair_it<pack < T, N>>
+  ifrexp_(BOOST_SIMD_SUPPORTS(simd_)
+        , pack<T,N> const& a0) BOOST_NOEXCEPT
+  {
+    auto that = raw_(ifrexp)(a0);
+    return  pair_it<pack<T,N>>{ if_else_zero(a0,that.first), if_else_zero(a0,that.second) };
+  }
+
+  //================================================================================================
+  // pedantic case
+  template<typename T, std::size_t N
+           ,  typename =  typename std::enable_if<std::is_floating_point<T>::value>
+  >
+  BOOST_FORCEINLINE pair_it<pack < T, N>>
+  ifrexp_(BOOST_SIMD_SUPPORTS(simd_)
+        , pedantic_tag const&
+        , pack<T,N> const& a0) BOOST_NOEXCEPT
+  {
+    using p_t = pack<T,N>;
 #ifndef BOOST_SIMD_NO_DENORMALS
-      auto test = logical_and(is_less(bs::abs(a0), Smallestposval<A0>()), is_nez(a0));
-      auto    t = if_else_zero(test,Nbmantissabits<A0>());
-      auto  aa0 = if_else(test, Twotonmb<A0>()*a0, a0);
+    auto test = logical_and(is_less(bs::abs(a0), Smallestposval<p_t>()), is_nez(a0));
+    auto    t = if_else_zero(test,Nbmantissabits<p_t>());
+    auto  aa0 = if_else(test, Twotonmb<p_t>()*a0, a0);
 #else
-      auto  aa0 = a0;
+    auto  aa0 = a0;
 #endif
 
-      auto r1 = simd::bitwise_cast<i_t>(bitwise_and(aa0, Mask1frexp<A0>())); //extract exp.
-      auto x  = bitwise_andnot(aa0, Mask1frexp<A0>());
-           r1 = shr(r1,Nbmantissabits<s_type>()) - Maxexponentm1<A0>();
-      auto r0 = bitwise_or(x,Mask2frexp<A0>());
+    auto r1 = simd::bitwise_cast<si_t<p_t>>(bitwise_and(aa0, Mask1frexp<p_t>())); //extract exp.
+    auto x  = bitwise_andnot(aa0, Mask1frexp<p_t>());
+    r1 = shr(r1,Nbmantissabits<T>()) - Maxexponentm1<p_t>();
+    auto r0 = bitwise_or(x,Mask2frexp<p_t>());
 
-      auto test0 = is_nez(aa0);
-      auto test1 = is_greater(r1,Limitexponent<A0>());
-              r1 = if_else_zero(logical_notand(test1, test0), r1);
+    auto test0 = is_nez(aa0);
+    auto test1 = is_greater(r1,Limitexponent<p_t>());
+    r1 = if_else_zero(logical_notand(test1, test0), r1);
 
 #ifndef BOOST_SIMD_NO_DENORMALS
-      r1 -= t ;
+    r1 -= t ;
 #endif
 
-      return  { if_else_zero(test0, if_plus(test1,r0,aa0))
-              , r1
-              };
-    }
-  };
+    return  pair_it<pack < T, N>>{ if_else_zero(test0, if_plus(test1,r0,aa0)), r1};
+  }
 
-  BOOST_DISPATCH_OVERLOAD( ifrexp_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pack_< bd::floating_<A0>, X>
-                          )
-  {
-    using i_t = bd::as_integer_t<A0, signed>;
 
-    BOOST_FORCEINLINE std::pair<A0,i_t> operator()(A0 const& a0) const BOOST_NOEXCEPT
-    {
-      auto that = raw_(ifrexp)(a0);
-      return  { if_else_zero(a0,that.first), if_else_zero(a0,that.second) };
-    }
-  };
 
-  BOOST_DISPATCH_OVERLOAD( ifrexp_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::raw_tag
-                          , bs::pack_< bd::floating_<A0>, X>
-                          )
-  {
-    using i_t = bd::as_integer_t<A0, signed>;
+//   //================================================================================================
+//   // std case
+//   template<typename T, std::size_t N
+//            ,  std::enable_if<std::is_floating_point<T>::value>
+//   >
+//   BOOST_FORCEINLINE pair_it<pack<T,N>>
+//   ifrexp_(BOOST_SIMD_SUPPORTS(simd_)
+//         , std_tag const&
+//         , pack<T,N> const& a0) BOOST_NOEXCEPT
+//   {
+//     return ???
+//   }
 
-    BOOST_FORCEINLINE std::pair<A0,i_t> operator()(bs::raw_tag const&,A0 const& a0) const BOOST_NOEXCEPT
-    {
-      auto m1f = Mask1frexp<A0>();
-      auto r1 = bitwise_cast<i_t>(bitwise_and(m1f, a0));
-      auto x  = bitwise_andnot(a0, m1f);
-
-      return  { bitwise_or(x,Mask2frexp<A0>())
-              , shr(r1,Nbmantissabits<typename A0::value_type>()) - Maxexponentm1<A0>()
-              };
-    }
-  };
 } } }
 
 #endif

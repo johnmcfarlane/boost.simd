27157ae995c0b64a8d5bcf46b94d8054ef7b6ba4
jtlap <jtlapreste@gmail.com>
2017-06-14 08:20:39 +0200

    ilogb sse2 case forgotten

--- include/boost/simd/arch/x86/sse2/simd/function/ilogb.hpp
+++ include/boost/simd/arch/x86/sse2/simd/function/ilogb.hpp
@@ -11,44 +11,31 @@
 #ifndef BOOST_SIMD_ARCH_X86_SSE2_SIMD_FUNCTION_ILOGB_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_X86_SSE2_SIMD_FUNCTION_ILOGB_HPP_INCLUDED
 
+#include <boost/simd/detail/pack.hpp>
 #include <boost/simd/detail/overload.hpp>
 #include <boost/simd/function/abs.hpp>
 #include <boost/simd/function/bitwise_and.hpp>
 #include <boost/simd/function/bitwise_cast.hpp>
 #include <boost/simd/function/dec.hpp>
-#include <boost/simd/function/exponent.hpp>
-#include <boost/simd/function/group.hpp>
 #include <boost/simd/function/if_inc.hpp>
-#include <boost/simd/function/split.hpp>
-#include <boost/simd/function/if_else.hpp>
-#include <boost/simd/function/if_zero_else.hpp>
-#include <boost/simd/function/is_lez.hpp>
 #include <boost/simd/function/is_nez.hpp>
 #include <boost/simd/function/shift_right.hpp>
-#include <boost/simd/function/tofloat.hpp>
 #include <boost/simd/constant/one.hpp>
 #include <boost/simd/constant/constant.hpp>
-#include <boost/simd/constant/zero.hpp>
 #include <boost/simd/detail/dispatch/meta/upgrade.hpp>
 
-namespace boost { namespace simd { namespace ext
+namespace boost { namespace simd { namespace detail
 {
-  namespace bd =  boost::dispatch;
-  namespace bs =  boost::simd;
-
-  BOOST_DISPATCH_OVERLOAD ( ilogb_
-                          , (typename A0)
-                          , bs::sse2_
-                          , bs::pack_<bd::uint8_<A0>, bs::sse_>
-                          )
+  BOOST_FORCEINLINE pack<uint8_t,16,sse_>
+  ilogb_( BOOST_SIMD_SUPPORTS(sse2_)
+        , pack<uint8_t,16,sse_> const& a0
+        ) BOOST_NOEXCEPT
   {
-
-    BOOST_FORCEINLINE A0 operator() ( const A0 & a0)
-    {
-      using ui16_t =  bd::upgrade_t<A0>;
-      const A0 mask =  Constant<A0, 0x7f>();
-      A0 n = a0;
-      A0 i = One<A0>();
+      using p_t =  pack<uint8_t,16,sse_>;
+      using ui16_t =  bd::upgrade_t<p_t>;
+      const p_t mask =  Constant<p_t, 0x7f>();
+      p_t n = a0;
+      p_t i = One<p_t>();
       n = bitwise_and(mask, shift_right(bitwise_cast<ui16_t>(n), 1));
       i = if_inc(is_nez(n), i);
       n = bitwise_and(mask, shift_right(bitwise_cast<ui16_t>(n), 1));
@@ -64,21 +51,16 @@ namespace boost { namespace simd { namespace ext
       n = bitwise_and(mask, shift_right(bitwise_cast<ui16_t>(n), 1));
       i = if_inc(is_nez(n), i);
       return dec(i);
-    }
-  };
-  BOOST_DISPATCH_OVERLOAD ( ilogb_
-                          , (typename A0)
-                          , bs::sse2_
-                          , bs::pack_<bd::int8_<A0>, bs::sse_>
-                          )
-  {
+  }
 
-    BOOST_FORCEINLINE A0 operator() ( const A0 & a0)
-    {
-      using uA0 = bd::as_integer_t<A0, unsigned>;
-      return bitwise_cast<A0>(ilogb(bitwise_cast<uA0>(saturated_(abs)(a0))));
-    }
-  };
+  BOOST_FORCEINLINE pack<int8_t,16,sse_>
+  ilogb_( BOOST_SIMD_SUPPORTS(sse2_)
+        , pack<int8_t,16,sse_> const& a0
+        ) BOOST_NOEXCEPT
+  {
+      using p_t =  pack<int8_t,16,sse_>;
+      return bitwise_cast<p_t>(ilogb(bitwise_cast<ui_t<p_t>>(saturated_(abs)(a0))));
+  }
 
 } } }
 
--- include/boost/simd/function/simd/ilogb.hpp
+++ include/boost/simd/function/simd/ilogb.hpp
@@ -13,10 +13,10 @@
 #include <boost/simd/function/scalar/ilogb.hpp>
 #include <boost/simd/arch/common/simd/function/ilogb.hpp>
 
-// #if defined(BOOST_HW_SIMD_X86_OR_AMD_AVAILABLE)
-// #  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_SSE2_VERSION
-// #    include <boost/simd/arch/x86/sse2/simd/function/ilogb.hpp>
-// #  endif
-// #endif
+#if defined(BOOST_HW_SIMD_X86_OR_AMD_AVAILABLE)
+#  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_SSE2_VERSION
+#    include <boost/simd/arch/x86/sse2/simd/function/ilogb.hpp>
+#  endif
+#endif
 
 #endif

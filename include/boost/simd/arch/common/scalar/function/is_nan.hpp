d2bd0e83a802eadc8bd3b1eb03bb547bf45cb8ae
jtlap <jtlapreste@gmail.com>
2017-06-20 16:21:01 +0200

    is_nan

--- include/boost/simd/arch/common/scalar/function/is_nan.hpp
+++ include/boost/simd/arch/common/scalar/function/is_nan.hpp
@@ -12,75 +12,65 @@
 #define BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_IS_NAN_HPP_INCLUDED
 #include <boost/simd/function/std.hpp>
 
+#include <boost/simd/constant/false.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
 #include <boost/simd/logical.hpp>
-#include <boost/simd/detail/dispatch/function/overload.hpp>
 #include <boost/config.hpp>
+#include <type_traits>
 #include <cmath>
 
-namespace boost { namespace simd { namespace ext
+namespace boost { namespace simd { namespace detail
 {
-  namespace bd = boost::dispatch;
-  namespace bs = boost::simd;
-  BOOST_DISPATCH_OVERLOAD ( is_nan_
-                          , (typename A0)
-                          , bd::cpu_
-                          , bd::scalar_< bd::bool_<A0> >
-                          )
+  BOOST_FORCEINLINE bool
+  is_nan_ ( BOOST_SIMD_SUPPORTS(cpu_)
+          , bool
+          ) BOOST_NOEXCEPT
   {
-    BOOST_FORCEINLINE bool operator()( const A0&) const BOOST_NOEXCEPT
-    {
-      return false;
-    }
-  };
-  BOOST_DISPATCH_OVERLOAD ( is_nan_
-                          , (typename A0)
-                          , bd::cpu_
-                          , bd::scalar_< bd::fundamental_<A0> >
-                          )
-  {
-    BOOST_FORCEINLINE logical<A0> operator()( const A0&) const BOOST_NOEXCEPT
-    {
-      return {false};
-    }
-  };
-  BOOST_DISPATCH_OVERLOAD ( is_nan_
-                          , (typename A0)
-                          , bd::cpu_
-                          , bd::scalar_< bd::floating_<A0> >
-                          )
-  {
-    BOOST_FORCEINLINE logical<A0> operator() ( A0 a0) const BOOST_NOEXCEPT
-    {
-      return  (a0 != a0);
-    }
-  };
-  BOOST_DISPATCH_OVERLOAD ( is_nan_
-                          , (typename A0)
-                          , bd::cpu_
-                          , bs::std_tag
-                          , bd::scalar_< bd::floating_<A0> >
-                          )
-  {
-    BOOST_FORCEINLINE logical<A0> operator() (const std_tag &,  A0 a0
-                                      ) const BOOST_NOEXCEPT
-    {
-      return std::isnan(a0);
-    }
-  };
+    return false;
+  }
 
-  BOOST_DISPATCH_OVERLOAD ( is_nan_
-                          , (typename A0)
-                          , bd::cpu_
-                          , bs::std_tag
-                          , bd::scalar_< bd::integer_<A0> >
-                          )
+  template <typename T>
+  BOOST_FORCEINLINE as_logical_t<T>
+  s_is_nan_( T a0
+           , std::true_type const &
+           ) BOOST_NOEXCEPT
   {
-    BOOST_FORCEINLINE logical<A0> operator() (const std_tag &,  A0
-                                             ) const BOOST_NOEXCEPT
-    {
-      return {false};
-    }
-  };
+    return  (a0!= a0);
+  }
+
+  template <typename T>
+  BOOST_FORCEINLINE as_logical_t<T>
+  s_is_nan_( T
+           , std::false_type const &
+           ) BOOST_NOEXCEPT
+  {
+    return False<T>();
+  }
+
+  template <typename T
+            , typename =  typename std::enable_if<std::is_arithmetic<T>::value>
+  >
+  BOOST_FORCEINLINE as_logical_t<T>
+  is_nan_( BOOST_SIMD_SUPPORTS(cpu_)
+         , T a0
+         ) BOOST_NOEXCEPT
+  {
+    return s_is_nan_(a0, std::is_floating_point<T>());
+  }
+
+  template <typename T
+            , typename =  typename std::enable_if<std::is_arithmetic<T>::value>
+  >
+  BOOST_FORCEINLINE as_logical_t<T>
+  is_nan_(BOOST_SIMD_SUPPORTS(cpu_)
+         , std_tag const &
+         , T a0
+         ) BOOST_NOEXCEPT
+  {
+    return std::isnan(a0);
+  }
+
 } } }
 
 
--- include/boost/simd/arch/common/simd/function/is_nan.hpp
+++ include/boost/simd/arch/common/simd/function/is_nan.hpp
@@ -12,41 +12,62 @@
 #define BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_IS_NAN_HPP_INCLUDED
 #include <boost/simd/detail/overload.hpp>
 
-#include <boost/simd/meta/hierarchy/simd.hpp>
+#include <boost/simd/detail/pack.hpp>
 #include <boost/simd/constant/false.hpp>
 #include <boost/simd/constant/zero.hpp>
 #include <boost/simd/function/is_unord.hpp>
 
-namespace boost { namespace simd { namespace ext
+namespace boost { namespace simd { namespace detail
 {
-   namespace bd = boost::dispatch;
-   namespace bs = boost::simd;
-   BOOST_DISPATCH_OVERLOAD_IF(is_nan_
-                          , (typename A0, typename X)
-                          , (detail::is_native<X>)
-                          , bd::cpu_
-                          , bs::pack_<bd::arithmetic_<A0>, X>
-                          )
-   {
-     using result = bs::as_logical_t<A0>;
-     BOOST_FORCEINLINE result operator()(const A0&) const
-      {
-        return bs::False<result>();
-      }
-   };
+  // Native implementation
+  template<typename T, std::size_t N>
+  BOOST_FORCEINLINE
+  auto v_is_nan_ ( pack<T,N> const& a0
+                 , std::true_type const &
+                 ) BOOST_NOEXCEPT_DECLTYPE_BODY
+  (
+    (is_unord(a0, a0))
+  )
 
-   BOOST_DISPATCH_OVERLOAD_IF(is_nan_
-                          , (typename A0, typename X)
-                          , (detail::is_native<X>)
-                          , bd::cpu_
-                          , bs::pack_<bd::floating_<A0>, X>
-                          )
-   {
-      BOOST_FORCEINLINE bs::as_logical_t<A0>  operator()( const A0& a0) const BOOST_NOEXCEPT
-      {
-         return is_unord(a0, a0);
-      }
-   };
+    template<typename T, std::size_t N>
+  BOOST_FORCEINLINE
+  auto v_is_nan_ ( pack<T,N> const&
+                 , std::false_type const &
+                 ) BOOST_NOEXCEPT_DECLTYPE_BODY
+  (
+    (False<pack<T,N>>())
+  )
+
+    template<typename T, std::size_t N>
+  BOOST_FORCEINLINE
+  auto is_nan_ ( BOOST_SIMD_SUPPORTS(simd_)
+               , pack<T,N> const& a
+               ) BOOST_NOEXCEPT_DECLTYPE_BODY
+  (
+    v_is_nan_(a, std::is_floating_point<T>())
+  )
+
+  // Emulated implementation
+    template<typename T, std::size_t N>
+  BOOST_FORCEINLINE
+  auto is_nan_ ( BOOST_SIMD_SUPPORTS(simd_)
+               , pack<T,N,simd_emulation_> const& a
+               ) BOOST_NOEXCEPT_DECLTYPE_BODY
+  (
+    map_to( simd::is_nan, a)
+  )
+
+  // std decoator
+    template<typename T, std::size_t N>
+  BOOST_FORCEINLINE
+  auto is_nan_ ( BOOST_SIMD_SUPPORTS(simd_)
+               , std_tag const &
+               , pack<T,N> const& a
+               ) BOOST_NOEXCEPT_DECLTYPE_BODY
+  (
+    map_to( std_(simd::is_nan), a)
+  )
+
 
 } } }
 

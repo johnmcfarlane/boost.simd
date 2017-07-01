d9edb9c9e4a3e26a2cb9b9a6c5f8ba44a021ece3
jtlap <jtlapreste@gmail.com>
2017-07-01 21:24:19 +0200

    refine_rec

--- include/boost/simd/arch/common/scalar/function/refine_rec.hpp
+++ include/boost/simd/arch/common/scalar/function/refine_rec.hpp
@@ -8,31 +8,21 @@
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_REFINE_REC_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_REFINE_REC_HPP_INCLUDED
-
-#include <boost/simd/detail/overload.hpp>
-// #include <boost/simd/function/fnms.hpp>
-// #include <boost/simd/function/fma.hpp>
+#include <boost/simd/function/fnms.hpp>
+#include <boost/simd/function/fma.hpp>
 #include <boost/simd/constant/one.hpp>
 
-namespace boost { namespace simd { namespace ext
+namespace boost { namespace simd { namespace detail
 {
-  namespace bd = boost::dispatch;
-  namespace bs = boost::simd;
-
-  BOOST_DISPATCH_OVERLOAD ( refine_rec_
-                          , (typename T)
-                          , bd::cpu_
-                          , bd::scalar_<bd::floating_<T>>
-                          , bd::scalar_<bd::floating_<T>>
-                          )
+  template<typename T, typename Arch>
+  BOOST_FORCEINLINE
+  T refine_rec_(BOOST_SIMD_SUPPORTS(Arch)
+               , T const& a0) BOOST_NOEXCEPT
   {
-    BOOST_FORCEINLINE T operator()(T a0, T x) const BOOST_NOEXCEPT
-    {
-      // Newton-Raphson: 1/X ~= x*(1-a0*x) + x
-//      return fma(fnms(x, a0, One<T>()), x, x);
-      return x*(1-a0*x) + x;
-    }
-  };
+    // Newton-Raphson: 1/X ~= x*(1-a0*x) + x
+    return fma(fnms(x, a0, One<T>()), x, x);
+  }
+
 } } }
 
 #endif
--- include/boost/simd/function/definition/refine_rec.hpp
+++ include/boost/simd/function/definition/refine_rec.hpp
@@ -12,25 +12,11 @@
 #define BOOST_SIMD_FUNCTION_DEFINITION_REFINE_REC_HPP_INCLUDED
 
 #include <boost/simd/config.hpp>
-#include <boost/simd/detail/dispatch/function/make_callable.hpp>
-#include <boost/simd/detail/dispatch/hierarchy/functions.hpp>
-#include <boost/simd/detail/dispatch.hpp>
+#include <boost/simd/detail/overload.hpp>
 
 namespace boost { namespace simd
 {
-  namespace tag
-  {
-    BOOST_DISPATCH_MAKE_TAG(ext, refine_rec_, boost::dispatch::elementwise_<refine_rec_>);
-  }
-
-  namespace ext
-  {
-    BOOST_DISPATCH_FUNCTION_DECLARATION(tag, refine_rec_)
-  }
-
-  BOOST_DISPATCH_CALLABLE_DEFINITION(tag::refine_rec_,refine_rec);
-
-
+  BOOST_SIMD_MAKE_CALLABLE(refine_rec_,refine_rec);
 } }
 
 #endif
